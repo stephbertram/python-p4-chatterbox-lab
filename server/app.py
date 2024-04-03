bd88c534953bfb6074d649f004ef8f912f626505
Stephanie Bertram <steph.a.bertram@gmail.com>
2024-04-02 19:25:09 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -7,20 +7,77 @@ from models import db, Message
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 app.json.compact = False
 
-CORS(app)
+cors = CORS(app)
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/messages')
+# whitelisting GET & POST
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        # messages = []
+        # don't need the .all() in Message.query.all() to create a list because creating list above
+        # for message in Message.query:
+        #     message_dict = message.to_dict()
+        #     messages.append(message_dict)
 
-@app.route('/messages/<int:id>')
+        messages = [message.to_dict() for message in Message.query.order_by("created_at")]
+        response = make_response(messages, 200)
+        return response
+    elif request.method == 'POST':
+        new_message = Message(
+            # first get json representation of data, then can .get
+            # in the future when have a lot of attributes will want to spread the data
+            body=request.get_json().get("body", "Body of message was not present"),
+            username=request.get_json().get("username"),
+        )
+        db.session.add(new_message)
+        db.session.commit()
+
+        response = make_response(new_message.to_dict(), 201)
+        return response
+    
+        # else:
+        # data = request.json
+        # new_message = Message(
+        #     username=data.get("username"), 
+        #     body=data.get("body")
+        # )
+        # db.session.add(new_message)
+        # db.session.commit()
+        # return new_message.to_dict(), 201
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = db.session.get(Message, id)
+    if message:
+        if request.method == 'DELETE':
+        
+                db.session.delete(message)
+                db.session.commit()
+                # don't send anything back because the delete worked
+                return "", 204
+        else:
+            if message:
+                data = request.json
+                # get is safest way to extract from a dictionary, can add a default
+                for attr, value in data.items():
+                    #setattr(object, attribute, value)
+                    setattr(message, attr, value) #! model validation always kick in when resetting attribute value
+                db.session.commit() #! db constraints
+                return message.to_dict(), 200
+    else:
+        return {"error": f"Message with id {id} not found."}, 404
 
+
+
+
+# Always run in debug! python app.py
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
+
+# import ipdb; ipdb.set_trace()
--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, DateTime, func
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
@@ -12,3 +12,17 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, onupdate=func.now())
+
+    def __repr__(self):
+        return f"""
+            <Message #{self.id}:
+                Body: {self.body},
+                Username: {self.username},
+                Created At: {self.created_at}
+            />
+        """
+ 
